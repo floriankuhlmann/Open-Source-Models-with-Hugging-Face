caee442aa04ce0f6751312f5a3fe0a70b51e04b9
Florian Kuhlmann <fk@neuesignale.com>
2024-06-12 18:01:13 +0200

    main.py update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 import argparse
+from model_classes.gradiospeech_model import GradioSpeechModel
+from model_classes.librispeech_model import LibrispeechModel
 from model_classes.ashraqesc50_model import AshraqEsc50Model
 from model_classes.minilml6_model import MiniLmL6Model
 from model_classes.nllb200_model import Nllb200Model
@@ -6,7 +8,7 @@ from model_classes.blenderbot_model import BlenderBotModel
 from transformers.utils import logging
 
 def main(model_name: str):
-    print("Hello, let's play!")
+    print("Hello, let's play with a hugging face!")
     logging.set_verbosity_error()
 
     # Wählen Sie das Modell basierend auf dem Parameter
@@ -18,6 +20,16 @@ def main(model_name: str):
         model = MiniLmL6Model() 
     elif model_name == "ashraqesc50":
         model = AshraqEsc50Model()
+    elif model_name == "librispeech":
+        ## course chapter
+        # https://learn.deeplearning.ai/courses/open-source-models-hugging-face/lesson/7/automatic-speech-recognition
+        # first part of the course chapter
+        model = LibrispeechModel()
+    elif model_name == "gradiospeech":
+        ## course chapter
+        # https://learn.deeplearning.ai/courses/open-source-models-hugging-face/lesson/7/automatic-speech-recognition
+        # second part of the course chapter
+        model = GradioSpeechModel()
     else:
         raise ValueError(f"Unbekanntes Modell: {model_name}")
 
